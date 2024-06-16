d907d14e7e197d7540d53a28ff6ca96e78515e5a
Ethan <ejo2117@columbia.edu>
2024-06-16 17:31:32 -0400

    5.1: ray-sphere intersection

--- main.cc
+++ main.cc
@@ -4,16 +4,23 @@
 
 #include <iostream>
 
-color lerp(const double a, const color start, const color end) {
-    return (1.0 - a) * start + a * end;
+bool hit_sphere(const point3& center, double radius, const ray& r) {
+    vec3 oc = center - r.origin();
+    auto a = dot(r.direction(), r.direction());
+    auto b = -2.0 * dot(r.direction(), oc);
+    auto c = dot(oc, oc) - radius*radius;
+    auto discriminant = b*b - 4*a*c;
+    return (discriminant >= 0);
 }
 
+
 color ray_color(const ray& r) {
+    if (hit_sphere(point3(0,0,-1), 0.5, r))
+        return color(1, 0, 0);
+
     vec3 unit_direction = unit_vector(r.direction());
     auto a = 0.5*(unit_direction.y() + 1.0);
-    auto startValue = color(1.0, 1.0, 1.0);
-    auto endValue = color(0.5, 0.7, 1.0);
-    return lerp(a, startValue, endValue);
+    return (1.0-a)*color(1.0, 1.0, 1.0) + a*color(0.5, 0.7, 1.0);
 }
 
 int main() {
